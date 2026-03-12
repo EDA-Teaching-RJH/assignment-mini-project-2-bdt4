569837629ae16d520e6f559cf1255f3df2a32b50
bdt4 <bdt4@kent.ac.uk>
2026-03-12 07:18:39 +0000

    my games list

--- game_backlog_tracker.py
+++ game_backlog_tracker.py
@@ -1,1 +1,2 @@
-#Primarily a steam game tracker, I'm in need of finishing a bunch of the games in my library, so I might as well get double the value from this project.
+#game backlog tracker, which allows me to double dip, using it as my project but also for my personal uses
+
